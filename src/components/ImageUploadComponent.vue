db26c57d3751b5a0524e9492a1fbc83078347569
Increative <awesome.commerce@yandex.ru>
2020-11-29 13:46:52 +0300

    final commit

--- src/components/ImageUploadComponent.vue
+++ src/components/ImageUploadComponent.vue
@@ -12,7 +12,8 @@
         outputFormat="verbose"
         @input="setImage">
         <label for="fileInput" slot="upload-label">
-          <button @click="clickButton()" style="margin-left: auto;margin-right: auto; margin-bottom: 2rem;">
+          <button @click="clickButton()"
+                  style="margin-left: auto;margin-right: auto; margin-bottom: 2rem;">
             Загрузить
           </button>
           <span class="upload-caption">{{
@@ -34,14 +35,40 @@
       };
     },
     methods: {
+      async findPerson(b64img) {
+        let response = await fetch(`https://crg-innotech-flask.herokuapp.com/find?img=${b64img}`);
+        let json = await response.json();
+        return json.id;
+      },
+      toDataURL(src, callback, outputFormat) {
+        var img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function () {
+          var canvas = document.createElement('CANVAS');
+          var ctx = canvas.getContext('2d');
+          var dataURL;
+          canvas.height = this.naturalHeight;
+          canvas.width = this.naturalWidth;
+          ctx.drawImage(this, 0, 0);
+          dataURL = canvas.toDataURL(outputFormat);
+          callback(dataURL);
+        };
+        img.src = src;
+        if (img.complete || img.complete === undefined) {
+          img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+          img.src = src;
+        }
+      },
+
       setImage: function (output) {
         this.hasImage = true;
         this.image = output;
         this.$emit('upload_image');
         console.log('Info', output.info);
         console.log('Exif', output.exif);
-        document.querySelectorAll('button')[0].style.display = 'none'
-        document.querySelectorAll('.upload-caption')[0].style.display = 'none'
+        document.querySelectorAll('button')[0].style.display = 'none';
+        document.querySelectorAll('.upload-caption')[0].style.display = 'none';
+
       },
       clickButton() {
         document.querySelectorAll('.upload-caption')[0].click();
